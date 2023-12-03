4d1dd2caa8080132f26954f61cd0ff4f0cdaf807
Nijia Lin <louis70109@gmail.com>
2023-12-03 16:20:05 +0800

    🔥 remove line bot requirement

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import base64
 import json
 import logging
 import os
-import sys
 
 if os.getenv('API_ENV') != 'production':
     from dotenv import load_dotenv
@@ -10,25 +9,10 @@ if os.getenv('API_ENV') != 'production':
     load_dotenv()
 
 
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from lotify.client import Client
 
-from linebot.v3.webhook import WebhookParser
-from linebot.v3.messaging import (
-    AsyncApiClient,
-    AsyncMessagingApi,
-    Configuration,
-    ReplyMessageRequest,
-    TextMessage,
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent,
-)
 
 import uvicorn
 
@@ -48,134 +32,6 @@ app.add_middleware(
 
 
 client = Client()
-
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-configuration = Configuration(
-    access_token=channel_access_token
-)
-
-async_api_client = AsyncApiClient(configuration)
-line_bot_api = AsyncMessagingApi(async_api_client)
-parser = WebhookParser(channel_secret)
-
-
-def order_flex(
-    title,
-    place,
-    time,
-    website,
-    image_url="https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-):
-    return {
-        "type": "bubble",
-        "hero": {
-            "type": "image",
-            "url": image_url,
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover",
-            "action": {
-                "type": "uri",
-                "uri": "http://linecorp.com/"
-            }
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": title,
-                    "weight": "bold",
-                    "size": "xl"
-                },
-                {
-                    "type": "box",
-                    "layout": "vertical",
-                    "margin": "lg",
-                    "spacing": "sm",
-                    "contents": [
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "Place",
-                                    "color": "#aaaaaa",
-                                    "size": "sm",
-                                    "flex": 1
-                                },
-                                {
-                                    "type": "text",
-                                    "text": place,
-                                    "wrap": True,
-                                    "color": "#666666",
-                                    "size": "sm",
-                                    "flex": 5
-                                }
-                            ]
-                        },
-                        {
-                            "type": "box",
-                            "layout": "baseline",
-                            "spacing": "sm",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "text": "Time",
-                                    "color": "#aaaaaa",
-                                    "size": "sm",
-                                    "flex": 1
-                                },
-                                {
-                                    "type": "text",
-                                    "text": time,
-                                    "wrap": True,
-                                    "color": "#666666",
-                                    "size": "sm",
-                                    "flex": 5
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-                {
-                    "type": "button",
-                    "style": "link",
-                    "height": "sm",
-                    "action": {
-                        "type": "uri",
-                        "label": "WEBSITE",
-                        "uri": website
-                    }
-                },
-                {
-                    "type": "box",
-                    "layout": "vertical",
-                    "contents": [],
-                    "margin": "sm"
-                }
-            ],
-            "flex": 0
-        }
-    }
 
 
 @app.post('/sub')
@@ -197,34 +53,6 @@ async def publisher(request: Request):
             access_token=os.environ.get('LOTIFY_ACCESS_TOKEN', 'xxx'),
             message=f"\n[NAME]\n訂單編號: {data.get('order_id')}\n訂單內容: {data.get('name')}")
         print(response)
-
-
-@app.post("/webhooks/line")
-async def handle_callback(request: Request):
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = await request.body()
-    body = body.decode()
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        raise HTTPException(status_code=400, detail="Invalid signature")
-
-    for event in events:
-        logging.info(event)
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessageContent):
-            continue
-        text = event.message.text
-        source = event.source
-        await line_bot_api.reply_message(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=str(source))]
-            ))
-    return 'OK'
 
 
 if __name__ == "__main__":
